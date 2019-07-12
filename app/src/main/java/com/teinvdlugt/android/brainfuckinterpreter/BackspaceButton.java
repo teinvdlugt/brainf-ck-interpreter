39dfbf84c4ef85caa5f7f84a0fb01b4f245d68ec
Tein van der Lugt <teinvdlugt@gmail.com>
2019-07-12 18:08:15 +0200

    Change icon and migrate to AndroidX

--- app/src/main/java/com/teinvdlugt/android/brainfuckinterpreter/BackspaceButton.java
+++ app/src/main/java/com/teinvdlugt/android/brainfuckinterpreter/BackspaceButton.java
@@ -1,7 +1,7 @@
 package com.teinvdlugt.android.brainfuckinterpreter;
 
 import android.content.Context;
-import android.support.v4.view.GestureDetectorCompat;
+import androidx.core.view.GestureDetectorCompat;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
